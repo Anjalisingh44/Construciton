e65f3deebb9e35769fdd431173271d247e0941fc
sujit Singh <fishermansujit@gmail.com>
2025-07-25 09:25:26 +0545

    what's app added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from './component/Navbar'
 import Home from './page/Home'
 import Footer from './component/Footer'
+import WhatsAppChat from './component/WhatsAppChat'
 const App = () => {
   return (
     <div >
       <Navbar/>
+      <WhatsAppChat/>
       <Home />
       <Footer/>
     </div>
